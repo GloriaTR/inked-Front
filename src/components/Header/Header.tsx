507c998c2cd29748918a2f8f0f02340535eeae94
Gloria Talavera <gloria.tare@gmail.com>
2023-11-09 12:18:00 +0100

    Delete logout button and Navigation from Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,6 @@
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase";
-import { signOut } from "firebase/auth";
-import Button from "../Button/Button";
-import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 const Header = (): React.ReactElement => {
-  const [user] = useAuthState(auth);
-
-  const logout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <header className="header">
       <div className="header-container">
@@ -25,18 +14,7 @@ const Header = (): React.ReactElement => {
           />
           <h1 className="header__title">inked</h1>
         </div>
-        {user && (
-          <Button className="button button-header" actionOnClick={logout}>
-            <img
-              src="/img/logout.svg"
-              alt="Logout button"
-              width="44"
-              height="44"
-            />
-          </Button>
-        )}
       </div>
-      {user && <Navigation />}
     </header>
   );
 };
