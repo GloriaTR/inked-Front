ea6b15c23aa8743820433ee0c4d7251a2ce6dbe4
Gloria Talavera <gloria.tare@gmail.com>
2024-01-11 15:51:38 +0100

    Create ScrollButton component and functionality

---
+++ src/components/ScrollButton/ScrollButton.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Button from "../Button/Button";
+import { FaAngleUp } from "react-icons/fa";
+
+const ScrollButton = (): React.ReactElement => {
+  const [showTopButton, setShowTopButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        setShowTopButton(true);
+      } else {
+        setShowTopButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      {showTopButton && (
+        <Button className="button button-position" actionOnClick={scrollToTop}>
+          <FaAngleUp className="icon-style" aria-label="Scroll to top icon" />
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default ScrollButton;
